6a04270c6a721785466962873eb02d551b2dc78b
mkorkmaz <korkmazgarcia@gmail.com>
2018-10-17 21:43:49 +0300

    Initial commit

---
+++ ClientApp/components/About.tsx
@@ -0,0 +1,17 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+
+export class About extends React.Component<RouteComponentProps<{}>, {}> {
+
+    public render(){
+
+        return (
+            <div>
+                <h1>Hi, I am Muhammed Korkmaz</h1>
+                <p>This is my first .net app with react js.</p>
+                <p>This code is writen by Intelij IDE</p>
+            </div>
+        );
+    }
+    
+}
